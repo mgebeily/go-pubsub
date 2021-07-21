c3ce4edcb2836d847300fee1658f1fc31940c311
Marc Gebeily <m.gebeily@gmail.com>
2021-07-21 19:57:30 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module go-ws-pubsub
+
+go 1.14
+
+require (
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/gofiber/fiber/v2 v2.14.0
+	github.com/gofiber/websocket/v2 v2.0.7
+	github.com/joho/godotenv v1.3.0
+	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/mgebeily/fiber-paseto v0.0.0-20210514015618-a20a542dbbb3
+	github.com/urfave/cli/v2 v2.3.0
+	github.com/valyala/fasthttp v1.27.0 // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+)
---
+++ main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+	"github.com/joho/godotenv"
+	pasetoware "github.com/mgebeily/fiber-paseto"
+)
+
+type Config struct {
+	ChannelLookup        string
+	ErrorHandler         string
+	PublishTokenLookup   string
+	SubscribeTokenLookup string
+
+	GetChannelSubscribers func(string) []chan string
+	CanJoinChannel        func(*fiber.Ctx, string) bool
+	PublishMiddleware     func(*fiber.Ctx) error
+	SubscribeMiddleware   func(*fiber.Ctx) error
+
+	AddToChannel      func(string) error
+	RemoveFromChannel func(string) error
+}
+
+func main() {
+	godotenv.Load()
+
+	// app := &cli.App{
+	// 	Name: "ws-pubsub",
+	// 	Flags: []cli.Flag{
+	// 		&cli.IntFlag{
+	// 			Name:  "port",
+	// 			Usage: "The port to start the server on.",
+	// 			Value: 3000,
+	// 		},
+	// 		&cli.StringFlag{
+	// 			Name:  "path",
+	// 			Usage: "The path to start the server on.",
+	// 			Value: "ws",
+	// 		},
+	// 		&cli.StringFlag{
+	// 			Name:  "subscribe-auth",
+	// 			Usage: "The type and location of the auth for subscribing to a channel.",
+	// 			Value: "paseto:header:token",
+	// 		},
+	// 		&cli.StringFlag{
+	// 			Name:  "publish-auth",
+	// 			Usage: "The type and location of the auth for publishing to a channel.",
+	// 			Value: "paseto:header:token",
+	// 		},
+	// 		&cli.StringFlag{
+	// 			Name:  "channel-lookup",
+	// 			Usage: "The location of the channel to operate on in the request.",
+	// 			Value: "header.token",
+	// 		},
+	// 	},
+	// 	Usage: "Start a pubsub server with the given options",
+	// 	Action: func(c *cli.Context) error {
+	// 		app := New(&Config{})
+	// 		return app.Listen(":" + c.String("port"))
+	// 	},
+	// }
+
+	// err := app.Run(os.Args)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	app := New(&Config{})
+	app.Listen(":3001")
+}
+
+// TODO: This with pure channels
+type Connection struct {
+	ChannelId  string
+	Subscriber chan string
+}
+
+// TODO: Just call this NEW and mount it in the app?
+func New(config *Config) *fiber.App {
+	app := fiber.New()
+
+	publisher := make(chan *Connection)
+	channels := make(map[string][]chan string)
+	if config.AddToChannel == nil {
+		config.AddToChannel = func(channelId string) error {
+			_, ok := channels[channelId]
+			if !ok {
+				channels[channelId] = []chan string{}
+			}
+
+			channels[channelId] = append(channels[channelId], make(chan string))
+
+			return nil
+		}
+	}
+	if config.GetChannelSubscribers == nil {
+		config.GetChannelSubscribers = func(channelId string) []chan string {
+			channel, ok := channels[channelId]
+			if !ok {
+				return []chan string{}
+			} else {
+				return channel
+			}
+		}
+	}
+	if config.SubscribeMiddleware == nil {
+		config.SubscribeMiddleware = func(c *fiber.Ctx) error {
+			return c.Next()
+		}
+	}
+	if config.PublishMiddleware == nil {
+		config.PublishMiddleware = func(c *fiber.Ctx) error {
+			return c.Next()
+		}
+	}
+
+	// TODO: After middleware?
+	app.Post("/publish/:channelId", config.PublishMiddleware, publishToChannel(config))
+
+	app.Use(func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+
+		return fiber.ErrUpgradeRequired
+	})
+	app.Get("/subscribe/:channelId",
+		config.SubscribeMiddleware,
+		func(c *fiber.Ctx) error {
+			channel := make(chan string)
+			publisher <- &Connection{ChannelId: c.Params("id"), Subscriber: channel}
+			c.Locals("channel", channel)
+			return c.Next()
+		},
+		subscribeToChannel(config))
+
+	go func() {
+		for channel := range publisher {
+			channels[channel.ChannelId] = append(channels[channel.ChannelId], channel.Subscriber)
+		}
+	}()
+
+	return app
+}
+
+func publishToChannel(config *Config) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		channels := config.GetChannelSubscribers(c.Params("id"))
+
+		for channel := range channels {
+			channels[channel] <- string(c.Body())
+		}
+
+		return c.SendStatus(200)
+	}
+}
+
+func subscribeToChannel(config *Config) fiber.Handler {
+	return websocket.New(func(conn *websocket.Conn) {
+		// TODO: Pass channel directly
+		subscriber := conn.Locals("channel").(chan string)
+
+		for value := range subscriber {
+			if err := conn.WriteMessage(websocket.TextMessage, []byte(value)); err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+	})
+}
+
+func createAuthMiddleware(tokenLookup string) func(c *fiber.Ctx) error {
+	// Split the strings into parts representing type and token lookup
+	parts := strings.Split(tokenLookup, ":")
+
+	switch parts[0] {
+	case "paseto":
+		return pasetoware.New(&pasetoware.Config{
+			TokenLookup: parts[1] + ":" + parts[2],
+		})
+	default:
+		return func(c *fiber.Ctx) error {
+			return c.Next()
+		}
+	}
+}
